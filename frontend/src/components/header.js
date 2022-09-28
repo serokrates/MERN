c72d205fe2a05cb15d152ae281c80900e8682315
serokrates <homm5@vp.pl>
2022-09-28 16:52:22 +0200

    logout on mobile

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -19,18 +19,7 @@ function Header() {
       <a class="navbar-brand" href="#">
         Menu
       </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <div id="navbarNavDropdown">
         <ul class="navbar-nav">
           {user ? (
             <li>
@@ -56,16 +45,6 @@ function Header() {
               </Link>
             </>
           )}
-          {/* <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown link
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li> */}
         </ul>
       </div>
     </nav>
